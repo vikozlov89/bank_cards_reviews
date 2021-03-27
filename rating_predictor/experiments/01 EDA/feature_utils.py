cbe1ee4ef141952b22af7baad91c77c5e68e6dc6
Vladislav Kozlov <vlad.kozlov.ds@gmail.com>
2021-03-27 11:30:43 +0200

    analysis progress

--- rating_predictor/experiments/01 EDA/feature_utils.py
+++ rating_predictor/experiments/01 EDA/feature_utils.py
@@ -1,12 +1,18 @@
 import re
 import pandas as pd
 import numpy as np
+import pymorphy2
+
+def apply_func_to_cols(df: pd.DataFrame, columns: list, suffix: str, func) -> pd.DataFrame:
+    tmp = df.copy()
+    
+    for colname in columns:
+        tmp[colname+suffix] = tmp[colname].map(func)
+    
+    return tmp
 
 def add_len(df: pd.DataFrame, text_columns: list) -> pd.DataFrame:
-    tmp = df.copy()
-    for colname in text_columns:
-        tmp[colname+'_len'] = tmp[colname].str.len()
-    return tmp
+    return apply_func_to_cols(df,text_columns,'_len',len)
 
 def count_pattern_share(pattern: str, text: str):
     if  text is np.nan or text is None:
@@ -19,3 +25,42 @@ def count_pattern_share(pattern: str, text: str):
 
 def count_caps_share(text: str) -> float:
     return count_pattern_share('[А-Я]', text)
+
+def count_stress_share(text: str) -> float:
+    return count_pattern_share('\!', text)
+
+def count_quest_share(text: str) -> float:
+    return count_pattern_share('\?', text)
+
+def add_share_caps(df: pd.DataFrame, text_columns: list) -> pd.DataFrame:
+    return apply_func_to_cols(df, text_columns, '_caps_share', count_caps_share)
+
+def add_share_stress(df: pd.DataFrame, text_columns: list) -> pd.DataFrame:
+    return apply_func_to_cols(df, text_columns, '_caps_share', count_stress_share)
+
+def add_share_quest(df: pd.DataFrame, text_columns: list) -> pd.DataFrame:
+    return apply_func_to_cols(df, text_columns, '_quest_share', count_quest_share)
+
+def replace_digits(text:str):
+    return re.sub('\d+','somedigits',text)
+
+def replace_variants(text, variants, replace_values)->str:
+    if len(variants) != len(replace_values):
+        raise Exception("varirants and replace_values must have the same length")
+    if not isinstance(variants,list):
+        raise Exception("varirants maust be a list")
+    if not isinstance(replace_values,list):
+        raise Exception("replace_values maust be a list")
+
+    res = text
+    for v, r in zip(variants, replace_values):
+        res = res.replace(v,r)
+    return res
+
+def replace_months(text: str)->str:
+    months = ['январь','февраль','март','апрель','май','июнь','июль','август','сентябрь','октябрь','ноябрь','декабрь']
+    return replace_variants(text, months,['somemonth']*12)
+
+def standartize_text(text: str, analyzer: pymorphy2.MorphAnalyzer) -> str:
+    res = [analyzer.normal_forms(w.lower())[0] for w in re.split("[\W\d]+",text)]
+    return ' '.join(res)
--- rating_predictor/experiments/01 EDA/test_feature_utils.py
+++ rating_predictor/experiments/01 EDA/test_feature_utils.py
@@ -1,9 +1,25 @@
 import pytest
-from feature_utils import count_caps_share
+from feature_utils import count_caps_share, replace_months
 import numpy as np
 
 @pytest.mark.parametrize("test_input,expected", [('АААааа',0.5), ('Я не знаю!', 0.1),('',0),('ни одной большой буквы',0),(np.nan,None),(None,None)])
 def test_count_caps_correctness(test_input, expected):
     assert count_caps_share(test_input) == expected
 
+@pytest.mark.parametrize("test_input,expected", [('какой-то текст','какой-то текст')
+                                               , ('пришел январь', 'пришел somemonth')
+                                               , ('пришел февраль', 'пришел somemonth')
+                                               , ('пришел март', 'пришел somemonth')
+                                               , ('пришел апрель', 'пришел somemonth')
+                                               , ('пришел май', 'пришел somemonth')
+                                               , ('пришел Май', 'пришел somemonth')
+                                               , ('пришел июнь', 'пришел somemonth')
+                                               , ('пришел июль', 'пришел somemonth')
+                                               , ('пришел август', 'пришел somemonth')
+                                               , ('пришел сентябрь', 'пришел somemonth')
+                                               , ('пришел октябрь', 'пришел somemonth')
+                                               , ('пришел ноябрь', 'пришел somemonth')
+                                               , ('пришел декабрь', 'пришел somemonth')]
+def test_replace_months(test_input, expected):
+    assert replace_months(test_input) == expected
 
